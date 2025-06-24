c04c39b7a562c79e2224b0a722b6a917611dd38d
Ashley Laisure <ashley.laisure@gmail.com>
2025-06-24 10:53:54 -0400

    init commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+            {/* theme toggle */}
+            {/* background effects */}
+            {/* Navbar */}
+            {/* Main Content */}
+            {/* Footer */}
+        </div>
+    )
+}
+
+export default Home
