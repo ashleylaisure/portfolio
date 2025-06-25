54a2ca0e7ff25142b27ac35e512a841cee361f43
Ashley Laisure <ashley.laisure@gmail.com>
2025-06-24 20:24:13 -0400

    background and navbar setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import NavBar from "../components/NavBar"
+import StarBackground from "../components/StarBackground"
+import ThemeToggle from "../components/ThemeToggle"
 
 const Home = () => {
     return (
         <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
             {/* theme toggle */}
+            <ThemeToggle />
             {/* background effects */}
+            <StarBackground />
             {/* Navbar */}
+            <NavBar />
             {/* Main Content */}
             {/* Footer */}
         </div>
