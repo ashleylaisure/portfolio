628c5fa53a738d17758749be2c11f3e2d9556246
Ashley Laisure <ashley.laisure@gmail.com>
2025-08-06 11:05:04 -0400

    update vite.config.js, tailwind.config.js, postcss.config.js, and index.html

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/portfolio/',
+  base: '/portfolio/',
   plugins: [react()],
   resolve:  {
     alias: {
